2099e8711f70589f28f46c465209cbf7239602b1
Yavuz Incedal <yavuzincedalca@gmail.com>
2021-09-13 16:30:12 -0400

    asserts

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+    }
+  };
+  
+  module.exports = assertEqual;
