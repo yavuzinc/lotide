e72e6de41026722a8b3c5c56bef7ae723d03dd70
Yavuz Incedal <yavuzincedalca@gmail.com>
2021-09-14 16:46:00 -0400

    merged eqArrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,11 @@
+const eqArrays = require("./eqArrays");
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+module.exports = assertArraysEqual;
