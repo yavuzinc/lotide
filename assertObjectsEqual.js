f9777f30d7d36c58db7d46663a21582fb5d22c83
Yavuz Incedal <yavuzincedalca@gmail.com>
2021-09-15 16:57:42 -0400

    assertObject

---
+++ assertObjectsEqual.js
@@ -0,0 +1,11 @@
+  
+const eqObjects = require("./eqObjects");
+
+const assertObjectEqual = function(object1, object2) {
+  const inspect = require('util').inspect;
+  if (eqObjects(object1, object2)) {
+    console.log(`✅  Assertion passed: ${inspect(object1)} === ${inspect(object2)}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${inspect(object1)} !== ${inspect(object2)}`);
+  }
+};
